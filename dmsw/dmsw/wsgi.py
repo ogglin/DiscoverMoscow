a3e03d042ec95c6624270358b59c16e584c94f1d
Server <server.ares@gmail.com>
2020-04-25 00:10:49 +0300

    wsgi

--- dmsw/dmsw/wsgi.py
+++ dmsw/dmsw/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmsw.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmsw.settings.production")
 
 application = get_wsgi_application()

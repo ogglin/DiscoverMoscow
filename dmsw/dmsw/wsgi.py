bd4c176d6130100885554a27518e94d2b6427782
Server <server.ares@gmail.com>
2020-04-26 10:26:41 +0300

    fix wsgi

--- dmsw/dmsw/wsgi.py
+++ dmsw/dmsw/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmsw.settings.production")
+sys.path.append('/var/www/DiscoverMoscow/dmsw')
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmsw.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmsw.settings.dev")
 
 application = get_wsgi_application()
